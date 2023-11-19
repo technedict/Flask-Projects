f22d4346bf55bc9f8b3fe77a6af6d547d5c202bd
Technedict <87270531+technedict@users.noreply.github.com>
2023-11-19 23:15:55 +0000

    Update memeServer.py

--- Meme App/memeServer.py
+++ Meme App/memeServer.py
@@ -7,7 +7,7 @@ import json
 app = Flask(__name__)
 
 def get_meme():
-    url = "https://meme-api.heroku.com/gimme"
+    url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
